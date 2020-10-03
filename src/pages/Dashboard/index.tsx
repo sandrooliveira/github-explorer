52bbc6d54246271cb55d0e7081176f7e8e6e8e49
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2020-10-03 14:27:36 -0300

    Add Dashboard form with styled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form } from './styles';
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explorer GitHub repositories</Title>
+
+      <Form>
+        <input placeholder="Type the repository name" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  )
 }
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Title = styled.h1`
+  font-size: 42px;
+  color: #3A3A3A;
+  max-width: 450px;
+  line-height: 56px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3A3A3A;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color:#fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
